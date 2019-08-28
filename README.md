40ae92318ecb27fc01eaf4e0bad23f2933f89f6a
Justin <iceyang1007@gmail.com>
2019-08-28 20:23:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+Reading collins text from clipboard, then formatting it and copying into clipboard.
+
+Required:
+
+- https://github.com/atotto/clipboard
+
+Build:
+
+```
+$ go get github.com/iceyang/go-collins
+```
+
+Run with:
+
+```Go
+gopaste | go-collins | gocopy
+```
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+const matchExp = "^(N|V|AD|PHRASE|→|CON|PREP)+"
+
+func main() {
+	result := ""
+	scanner := bufio.NewScanner(os.Stdin)
+	var text string
+	no := 1
+	for true {
+		scanner.Scan()
+		text = scanner.Text()
+		if len(text) == 0 {
+			break
+		}
+		ok, _ := regexp.MatchString(matchExp, text)
+		if ok {
+			text = fmt.Sprintf("\n%d. %s\n", no, text)
+			no++
+		}
+		result += text + "\n"
+	}
+	fmt.Print(result[1 : len(result)-1])
+}
